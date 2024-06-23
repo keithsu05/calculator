6e2af90c028b328776b8448a4f7b0ed13bbe53a1
L05TFuture <keithsu6705@gmail.com>
2024-06-23 18:33:29 +0800

    completed till step 4

---
+++ script.js
@@ -0,0 +1,42 @@
+const add =function(a,b){
+    return a+b
+    num
+
+}
+
+const subtract =function(a,b){
+    return a-b
+
+}
+
+const multiply = function(a,b){
+    return a*b
+
+}
+
+const divide =function(a,b){
+return a/b
+}
+
+let firstNum = ''
+let secondNum = ''
+let operator = ''
+
+const operate = function (firstNum,secondNum,operator){
+switch(operator){
+    case "+":
+        return add(firstNum,secondNum)
+        break;
+    case "-":
+        
+}
+
+}
+
+let displayValue = ''
+
+const populateDisplay = function(){
+    
+}
+
+
