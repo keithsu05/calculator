2b3ebacb5348931d994474e8ba9d12d80ffbf629
keithsu05 <morobollo@KeithsMacBook-Air.local>
2024-06-24 20:07:35 +0800

    finished step 5

--- script.js
+++ script.js
@@ -35,8 +35,34 @@ switch(operator){
 
 let displayValue = ''
 
-const populateDisplay = function(){
+const display = document.getElementById("display")
+
+const populateDisplay = function(digit){
+    display.textContent=digit
     
 }
+let numberArray = ["one","two","three","four","five","six","seven","eight","nine"]
 
+const createEventListener = function(){
+    //use a loop to iterate 9 times
+    // each iteration create an event listener for 
+    //each digit
+    for (let i=0;i<9;i++){
+        
+    
+        //need array for one-nine
+        //get the id of the element from the array
+        //use the id to define a variable in js
+        //add the event listener
+        const elementId = `${numberArray[i]}`
+        const elementSelect = document.getElementById(elementId);
+        elementSelect.addEventListener("click",()=>{
+            populateDisplay(i+1);
+        })
 
+        
+        
+    }
+}
+
+createEventListener()
