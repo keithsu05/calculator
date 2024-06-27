3bd8032c861204bcca2378aaad478a8937cb08e1
keithsu05 <morobollo@KeithsMacBook-Air.local>
2024-06-28 01:05:08 +0800

    fixed the equals and clear buttons. fimade functioning operator event listeners. enabled numbers>9 to be keyed in. Now working on run on calcualtions where an operator is pressed continuously instead of =

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const add =function(a,b){
+
     return a+b
-    num
+    
 
 }
 
@@ -18,29 +19,58 @@ const divide =function(a,b){
 return a/b
 }
 
-let firstNum = ''
-let secondNum = ''
+let displayValue = ''
+let memoryValue = ''
 let operator = ''
+let solution = ''
 
 const operate = function (firstNum,secondNum,operator){
 switch(operator){
-    case "+":
+    case "add":
         return add(firstNum,secondNum)
         break;
-    case "-":
+    case "subtract":
+        return subtract(firstNum,secondNum)
+        break;
+    case "multiply":
+        return multiply(firstNum,secondNum)
+        break;
+    case "divide":
+        return divide(firstNum,secondNum)
+        break;
         
 }
 
 }
 
-let displayValue = ''
+const equalsButton = document.getElementById("equals")
+equalsButton.addEventListener("click",()=>{
+    solution = operate(Number(displayValue),Number(memoryValue),operator)
+    display.textContent = solution
+    memoryValue = solution
+    displayValue=""
+})
+
+//somethings wrong with the equals button
+//i thinkits cuz the display and memory vlaues are calcualted as strings
+//fixed
+
+const clearButton = document.getElementById("clear")
+clearButton.addEventListener("click",()=>{
+    display.textContent = ""
+    displayValue =""
+    memoryValue=""
+    operator=''
+    solution=""
+} )
 
 const display = document.getElementById("display")
-
 const populateDisplay = function(digit){
-    display.textContent=digit
+    display.textContent+=digit
     
 }
+
+
 let numberArray = ["one","two","three","four","five","six","seven","eight","nine"
     
 ]
@@ -60,8 +90,9 @@ const createEventListener = function(){
         const elementSelect = document.getElementById(elementId);
         elementSelect.addEventListener("click",()=>{
             populateDisplay(i+1);
+            displayValue+=i+1
         })
-        displayValue=i+1
+        
 
         
         
@@ -69,3 +100,82 @@ const createEventListener = function(){
 }
 
 createEventListener()
+
+
+
+
+
+//when operator is clicked./
+const operatorArray = ["add","subtract","multiply","divide"]
+const createOperatorEventListener= function(){
+    
+    for (let i=0;i<3;i++){
+        
+    
+        //need array for one-nine
+        //get the id of the element from the array
+        //use the id to define a variable in js
+        //add the event listener
+        const elementId = `${operatorArray[i]}`
+        const elementSelect = document.getElementById(`${elementId}`);
+        elementSelect.addEventListener("click",()=>{
+            //when clicked, change operator variable 
+            //update first and second number
+        
+            operator= elementId
+            operationFunction()
+
+            display.textContent=solution
+        })
+    }
+}
+
+createOperatorEventListener()
+//when an operator is clicked with existing values under display and memory,
+//operator variable is updated
+//the operate function is carried out on those values first "what if its empty variable"
+//solution is then stored under memory
+//display value is returned to blank
+
+//if operator is clicked when there is no existing memory value.
+//operator value is updated
+//memory is updated to that of display value
+//display value is cleared for new incoming number
+//NO OPERATION IS PERFORMED
+
+
+//operator event listenr not wokring properly uhgugh
+
+let runOnCalculation = false
+const operationFunction = function(){
+if (displayValue && memoryValue) {
+    
+  solution = operate(Number(displayValue),Number(memoryValue),`${operator}`)
+  display.textContent=solution  
+  memoryValue = solution
+    displayValue =""
+    runOnCalculation = true
+} 
+else {
+   memoryValue=displayValue
+    displayValue=""
+}
+}
+
+const checkRunOnCalc = function(){
+    if(runOnCalculation==false){}
+    else{//on clicking new digit, display is replaced with numeber}
+        //
+}
+}
+
+//when an operator is clicked, any following number clicked should
+//replace and show upon the display whilst the previous number is stored
+//under memory
+// to fix this, create a true/false runOnCalualtion variable?
+
+        
+
+        
+        
+    
